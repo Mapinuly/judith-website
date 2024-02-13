ca394772232ddfcfd9641009bf35e7582c2f545a
yigal <yigal@THC6>
2024-02-13 21:58:23 +0200

    carousel

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -12,3 +12,18 @@ export const IMAGE_CONSTANTS={
     planet:'https://www.gogis.co.il/assets/img/planet.png',
     baseImg:"http://134.209.122.168/"
 }
+
+// export const IMAGE_CONSTANTS={
+//     logo:'https://www.gogis.co.il/assets/img/logo.png',
+//     google_cloud:'https://www.gogis.co.il/assets/img/google-cloud.png',
+//     google_maps:'https://www.gogis.co.il/assets/img/google-maps-platform.svg',
+//     google_workspace:'https://www.gogis.co.il/assets/img/google-workspace.svg',
+//     here:'https://www.gogis.co.il/assets/img/here.svg',
+//     mapBox:'https://www.gogis.co.il/assets/img/mapbox.svg',
+//     mapsIndoor:'https://www.gogis.co.il/assets/img/mapsindoors.svg',
+//     thermoplaye:'https://www.gogis.co.il/assets/img/thermopylae.png',
+//     carto:'https://www.gogis.co.il/assets/img/carto.svg',
+//     hexagon:'https://www.gogis.co.il/assets/img/Hexagon.png',
+//     planet:'https://www.gogis.co.il/assets/img/planet.png',
+//     baseImg:"http://134.209.122.168/"
+// }
