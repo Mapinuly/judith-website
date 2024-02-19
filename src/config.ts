484248b2bc09302d9fbd645dce7714aa16a12fb1
Mapinuly <mapinuly88@gmail.com>
2024-02-19 13:20:50 +0530

    updated code

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-    baseUrl: 'https://localhost:44343/api',
-    baseImg: 'https://localhost:44343/api',
-    // baseUrl: 'https://www.gogis.co.il/api',
-    // baseImg: 'https://www.gogis.co.il/api',
+    // baseUrl: 'https://localhost:44343/api',
+    // baseImg: 'https://localhost:44343/api',
+    baseUrl: 'https://www.gogis.co.il/api',
+    baseImg: 'https://www.gogis.co.il/api',
     cards: '/slider',
     weAreHere: '/we_are_here',
     team: '/team',
